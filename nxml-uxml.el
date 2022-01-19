a48ef8fd9c9782278973087e009d04aa3054b42c
Daphne Preston-Kendal <git@dpk.io>
2022-01-19 10:04:54 +0100

    Remove Org mode junk from Commentary

--- nxml-uxml.el
+++ nxml-uxml.el
@@ -25,11 +25,12 @@
 ;;
 ;;; Commentary:
 ;;
-;; [[https://dvcs.w3.org/hg/microxml/raw-file/tip/spec/microxml.html][MicroXML]] is a delightful tiny subset of XML, removing everything
-;; that makes XML a nightmare to deal with in practice. nxml-uxml-mode
-;; is a minor mode for Emacs that slightly modifies the XML parser of
-;; nxml-mode to forbid most (though, at present, not quite all)
-;; constructs which are allowed in XML 1.0 but disallowed in MicroXML.
+;;   MicroXML is a delightful tiny subset of XML, removing everything
+;;   that makes XML a nightmare to deal with in practice.
+;;   nxml-uxml-mode is a minor mode for Emacs that slightly modifies
+;;   the XML parser of nxml-mode to forbid most (though, at present,
+;;   not quite all) constructs which are allowed in XML 1.0 but
+;;   disallowed in MicroXML.
 ;;
 ;;; Code:
 
