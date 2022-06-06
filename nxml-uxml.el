95bbd0018ab218b9f39f5bf1f1e809f60fbc3edc
Daphne Preston-Kendal <git@dpk.io>
2022-06-06 14:13:58 +0200

    Use the correct form of with-demoted-errors 🤦🏼‍♀️

--- nxml-uxml.el
+++ nxml-uxml.el
@@ -81,23 +81,23 @@ MicroXML parsers.")
 Argument PROLOG is returned unchanged (this is advice for
 `xmltok-forward-prolog')."
   (when nxml-uxml-restrictions-p
-    (with-demoted-errors
-        (dolist (item prolog)
-          (let ((type (aref item 0))
-                (start (aref item 1))
-                (end (aref item 2)))
-            (cond ((eq type 'xml-declaration)
-                   (xmltok-add-error "XML declaration not allowed in MicroXML"
-                                     start end))
-                  ((eq type 'markup-declaration-open)
-                   (if (string= (buffer-substring start end) "<!DOCTYPE")
-                       (xmltok-add-error "DOCTYPE declaration not allowed in MicroXML"
-                                         start end)
-                     (xmltok-add-error "Declaration not allowed in MicroXML"
-                                       start end)))
-                  ((eq type 'processing-instruction-left)
-                   (xmltok-add-error "Processing instruction not allowed in MicroXML"
-                                     start end)))))))
+    (with-demoted-errors "µXML Error: %S"
+      (dolist (item prolog)
+        (let ((type (aref item 0))
+              (start (aref item 1))
+              (end (aref item 2)))
+          (cond ((eq type 'xml-declaration)
+                 (xmltok-add-error "XML declaration not allowed in MicroXML"
+                                   start end))
+                ((eq type 'markup-declaration-open)
+                 (if (string= (buffer-substring start end) "<!DOCTYPE")
+                     (xmltok-add-error "DOCTYPE declaration not allowed in MicroXML"
+                                       start end)
+                   (xmltok-add-error "Declaration not allowed in MicroXML"
+                                     start end)))
+                ((eq type 'processing-instruction-left)
+                 (xmltok-add-error "Processing instruction not allowed in MicroXML"
+                                   start end)))))))
   prolog)
 
 (defun nxml-uxml-disallow-gts (start end)
@@ -126,41 +126,41 @@ Argument PROLOG is returned unchanged (this is advice for
 Argument TOKEN is returned unchanged (this advice for
 `xmltok-forward')."
   (when nxml-uxml-restrictions-p
-    (with-demoted-errors
-        (cond ((eq xmltok-type 'start-tag)
-               (if xmltok-name-colon
-                   (xmltok-add-error "Colons are not allowed in element names in MicroXML"
-                                     xmltok-start xmltok-name-end))
-               (dolist (attr (append xmltok-attributes xmltok-namespace-attributes))
-                 (cond ((aref attr 1)
-                        (xmltok-add-error "Colons are not allowed in attribute names in MicroXML"
-                                          (aref attr 0) (aref attr 2)))
-                       ((string= (buffer-substring (aref attr 0) (aref attr 2))
-                                 "xmlns")
-                        (xmltok-add-error "Namespaces are not allowed in MicroXML")))
-                 (nxml-uxml-disallow-gts (aref attr 3) (aref attr 4))
-                 (if (not nxml-uxml-allow-newlines-in-attributes)
-                     (nxml-uxml-disallow-newlines (aref attr 3) (aref attr 4)))))
-              ((eq xmltok-type 'data)
-               (nxml-uxml-disallow-gts xmltok-start (point)))
-              ((eq xmltok-type 'cdata-section)
-               (xmltok-add-error "CDATA sections are not allowed in MicroXML"
-                                 xmltok-start))
-              ((eq xmltok-type 'char-ref)
-               (let ((end (point)))
-                 (save-excursion
-                   (goto-char xmltok-start)
-                   (cond ((not (looking-at "&#x"))
-                          (xmltok-add-error "Only hexadecimal character references are allowed in MicroXML" xmltok-start end))
-                         ((looking-at "&#x0*[dD];")
-                          (xmltok-add-error "Character reference to `&#xD;' not allowed in MicroXML" xmltok-start end))))))
-              ((eq xmltok-type 'entity-ref)
-               ;; in practice this should be caught by the fact doctypes are disallowed, but let's be extra careful
-               (let ((end (point)))
-                 (save-excursion
-                   (goto-char xmltok-start)
-                   (if (not (looking-at "&\\(amp\\|lt\\|gt\\|quot\\|apos\\);"))
-                       (xmltok-add-error "Entity reference not allowed in MicroXML" xmltok-start end))))))))
+    (with-demoted-errors "µXML Error: %S"
+      (cond ((eq xmltok-type 'start-tag)
+             (if xmltok-name-colon
+                 (xmltok-add-error "Colons are not allowed in element names in MicroXML"
+                                   xmltok-start xmltok-name-end))
+             (dolist (attr (append xmltok-attributes xmltok-namespace-attributes))
+               (cond ((aref attr 1)
+                      (xmltok-add-error "Colons are not allowed in attribute names in MicroXML"
+                                        (aref attr 0) (aref attr 2)))
+                     ((string= (buffer-substring (aref attr 0) (aref attr 2))
+                               "xmlns")
+                      (xmltok-add-error "Namespaces are not allowed in MicroXML")))
+               (nxml-uxml-disallow-gts (aref attr 3) (aref attr 4))
+               (if (not nxml-uxml-allow-newlines-in-attributes)
+                   (nxml-uxml-disallow-newlines (aref attr 3) (aref attr 4)))))
+            ((eq xmltok-type 'data)
+             (nxml-uxml-disallow-gts xmltok-start (point)))
+            ((eq xmltok-type 'cdata-section)
+             (xmltok-add-error "CDATA sections are not allowed in MicroXML"
+                               xmltok-start))
+            ((eq xmltok-type 'char-ref)
+             (let ((end (point)))
+               (save-excursion
+                 (goto-char xmltok-start)
+                 (cond ((not (looking-at "&#x"))
+                        (xmltok-add-error "Only hexadecimal character references are allowed in MicroXML" xmltok-start end))
+                       ((looking-at "&#x0*[dD];")
+                        (xmltok-add-error "Character reference to `&#xD;' not allowed in MicroXML" xmltok-start end))))))
+            ((eq xmltok-type 'entity-ref)
+             ;; in practice this should be caught by the fact doctypes are disallowed, but let's be extra careful
+             (let ((end (point)))
+               (save-excursion
+                 (goto-char xmltok-start)
+                 (if (not (looking-at "&\\(amp\\|lt\\|gt\\|quot\\|apos\\);"))
+                     (xmltok-add-error "Entity reference not allowed in MicroXML" xmltok-start end))))))))
   token)
 
 (provide 'nxml-uxml)
